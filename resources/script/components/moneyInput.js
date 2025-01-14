46dac1da38404d10ca0dddde619e41e48957c63a
PlayerDuvidoso <artuzao97pipas@gmail.com>
2025-01-14 19:05:04 -0300

    Code Cleanup, Updated formatInput() method, removed updateInput()

--- resources/script/components/moneyInput.js
+++ resources/script/components/moneyInput.js
@@ -5,6 +5,7 @@ class CurrencyInput {
     
     constructor(currency, id=null) {
         this.parser = new CurrencyParser(currency);
+
         this.inputElement = document.createElement("input");
         this.inputElement.classList.add("currency-dialog-input")
         this.inputElement.type = "text"
@@ -18,40 +19,31 @@ class CurrencyInput {
         this.inputElement.addEventListener("click", () => this.inputElement.focus());
 
     };
-    
-    resetInput() {
-        this.inputElement.setSelectionRange(-1, -1);
-        this.formatInput("000");
-    };
 
     inputEventHandler(Event) {
-        const newValueArr = this.parseInput(this).split("")
+        const newValueArr = this.extractInput()
 
         switch (Event.inputType) {
             case "insertText": //Insertion logic
                 if (newValueArr[0] == 0) {
                     newValueArr.shift()
-                    this.valueArr = newValueArr.join("")
-                    this.inputElement.value = this.valueArr
+                    this.inputElement.value = newValueArr
                 }
 
-                this.valueArr = newValueArr.join("")
-                this.formatInput(this.valueArr)
+                this.formatInput(newValueArr)
                 break;
             
             case "deleteContentBackward": //Deletion logic
                 if (newValueArr.length < 3) {
                     newValueArr.unshift("0")
-                    this.valueArr = newValueArr.join("")
-                    this.updateInput(this.valueArr)
+                    this.formatInput(newValueArr)
                 }
 
-                this.valueArr = newValueArr.join("")
-                this.formatInput(this.valueArr)
+                this.formatInput(newValueArr)
                 break;
             
             case "insertFromPaste": //Prevents pasting
-                this.formatInput(this.valueArr)    
+                this.formatInput(newValueArr)    
                 break;
         };
     };
@@ -80,22 +72,26 @@ class CurrencyInput {
     };
 
     formatInput(value) {
-        const formatedValue = this.parser.format(value)
-        this.updateInput(formatedValue)
+
+        if (!value) {
+            value = "000".split("")
+        }
+
+        const formatedValue = this.parser.format(value.join(""))
+        this.inputElement.value = formatedValue
     };
 
-    parseInput() {
-        return this.parser.extract(this.getValue())
+    resetInput() {
+        this.inputElement.setSelectionRange(-1, -1);
+        this.formatInput();
     };
-
+    
     getValue() {
         return this.inputElement.value
     };
-
-    //TODO: Merge parseInput and updateInput into one method
-
-    updateInput(value) {
-        this.inputElement.value = value
+    
+    extractInput() {
+        return this.parser.extract(this.getValue()).split("")
     };
 
     render() {
